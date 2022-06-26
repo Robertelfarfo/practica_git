3f197b8657a840b4c26367310f17e38326998cc2
Farfan <roberto1996farfan@hotmail.com>
2022-06-26 13:48:55 -0500

    Se crea un funcion para contar em pares

--- codigo.py
+++ codigo.py
@@ -2,6 +2,10 @@ def contador(frase = str):
     palabras = frase.split(" ")
     return "Numero de palabras: " + str(len(palabras))
 
+def contare_pares(begin = int, saltos = int):
+    for x in range(saltos):
+        print(begin + x * 2)
+    return
 
 if __name__ =="__main__":
     print("Da una frase")
