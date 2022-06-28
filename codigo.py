ea6050eb28aebb8d57396470df1872442793ca3c
Farfan <roberto1996farfan@hotmail.com>
2022-06-27 21:48:28 -0500

    Se agregu una funcionpara detectar si un numero es par o impar, ademas de que ene l main se manda llamar esta funcion

--- codigo.py
+++ codigo.py
@@ -7,7 +7,17 @@ def contare_pares(begin = int, saltos = int):
         print(begin + x * 2)
     return
 
+def par_impar(numero = int):
+    if numero % 2 == 0:
+        print("Es par")
+    else:
+        print("No es par")
+    return
+
+
 if __name__ =="__main__":
     print("Da una frase")
     contar = input()
     contador(contar)
+
+    par_impar(67)
